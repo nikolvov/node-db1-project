ae4abb1fee92e52e17fdc3fc683fd0d0b0c77223
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2022-06-20 13:59:30 -0700

    checks for unique name

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,10 +1,12 @@
 const Account = require('../accounts/accounts-model');
+const db = require('../../data/db-config');
 
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
   // console.log('checkAccountPayload middleware');
+  // next()
   const error = { status: 400 }
   const {name, budget} = req.body;
   if(name === undefined || budget === undefined) {
@@ -25,10 +27,22 @@ exports.checkAccountPayload = (req, res, next) => {
   }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
+exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
-  console.log('checkAccountNameUnique middleware');
-  next()
+  // console.log('checkAccountNameUnique middleware');
+  // next()
+  try{
+    const exisitng = await db('accounts')
+      .where('name', req.body.name.trim())
+      .first()
+    if(exisitng){
+      next({ status: 400, message: 'that name is taken'})
+    }else{
+      next()
+    }
+  }catch(err){
+    next(err)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
