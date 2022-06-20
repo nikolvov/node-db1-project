e73650da29acfcf0e8dd5af7bb210acb7b97af54
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2022-06-20 13:45:10 -0700

    fleshed out middleware error handling

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -4,7 +4,25 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
-  console.log('checkAccountPayload middleware');
+  // console.log('checkAccountPayload middleware');
+  const error = { status: 400 }
+  const {name, budget} = req.body;
+  if(name === undefined || budget === undefined) {
+    error.message = 'name and budget are required'
+    next(error)
+  }else if(typeof name !== 'string'){
+    error.message = 'name of account must be a string'
+    next(error)
+  }else if(name.trim().length < 3 || name.trim().length > 100) {
+    error.message = 'name of account must be between 3 and 100'
+    next(error)
+  }else if(typeof budget !== 'number' || isNaN(budget)) {
+    error.message = 'budget of account must be a number'
+    next(error)
+  }else if(budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
+    next(error)
+  }
   next()
 }
 
